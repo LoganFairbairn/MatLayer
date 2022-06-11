bb734abc643d35f86041cdb433365a83a1b3a1b6
Ryver <loganfairbairn@hotmail.com>
2022-06-11 12:57:23 -0400

    Deleting a layer now deletes nodes in all material channels.

--- layers/layer_operations/delete_layer.py
+++ layers/layer_operations/delete_layer.py
@@ -20,16 +20,18 @@ class COATER_OT_delete_layer(Operator):
         layer_stack = context.scene.coater_layer_stack
         selected_layer_index = context.scene.coater_layer_stack.layer_index
 
-        # Remove all nodes for all channels.
-        material_channel_node = material_channel_nodes.get_material_channel_node(context, "COLOR")
-        node_list = layer_nodes.get_all_nodes_in_layer(material_channel_node, layers, selected_layer_index)
-        for node in node_list:
-            material_channel_node.node_tree.nodes.remove(node)
+        # Remove all nodes for all material channels.
+        material_channel_list = material_channel_nodes.get_material_channel_list()
+        for material_channel in material_channel_list:
+            material_channel_node = material_channel_nodes.get_material_channel_node(context, material_channel)
+            node_list = layer_nodes.get_all_nodes_in_layer(material_channel_node, layers, selected_layer_index)
+            for node in node_list:
+                material_channel_node.node_tree.nodes.remove(node)
 
-        # Remove node frame if it exists.
-        frame = layer_nodes.get_layer_frame(material_channel_node, layers, selected_layer_index)
-        if frame != None:
-            material_channel_node.node_tree.nodes.remove(frame)
+            # Remove layer frame if it exists.
+            frame = layer_nodes.get_layer_frame(material_channel_node, layers, selected_layer_index)
+            if frame != None:
+                material_channel_node.node_tree.nodes.remove(frame)
 
         # Remove the layer from the list.
         layers.remove(layer_stack.layer_index)
@@ -39,12 +41,3 @@ class COATER_OT_delete_layer(Operator):
         update_layer_nodes.update_layer_nodes(context)
 
         return {'FINISHED'}
-
-def remove_color_channel_layer_nodes(context):
-    '''Delete all nodes for the color channel.'''
-    color_material_channel_node = material_channel_nodes.get_material_channel_node("BASECOLOR")
-
-    #if color_material_channel_node != None:
-    #   nodes = get_all_layer_nodes(context, layer_index)
-
-
