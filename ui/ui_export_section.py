a8d1e3fc219e0084c59ea071d037a9249ccbe0a3
Ryver <58080736+LoganFairbairn@users.noreply.github.com>
2023-04-04 11:22:24 -0400

    Updated the export section ui to be consistent with the baking ui layout.

--- ui/ui_export_section.py
+++ ui/ui_export_section.py
@@ -21,47 +21,56 @@ def draw_export_section_ui(self, context):
     row = layout.row()
     row.scale_y = scale_y
     row.prop(export_settings, "export_base_color", text="")
-    row.operator("matlay.export_base_color", text="Export Base Color", icon='RENDER_STILL')
+    row.label(text="Base Color")
+    row.operator("matlay.export_base_color", text="", icon='RENDER_STILL')
 
     row = layout.row()
     row.scale_y = scale_y
     row.prop(export_settings, "export_subsurface", text="")
-    row.operator("matlay.export_subsurface", text="Export Subsurface", icon='RENDER_STILL')
+    row.label(text="Subsurface")
+    row.operator("matlay.export_subsurface", text="", icon='RENDER_STILL')
 
     row = layout.row()
     row.scale_y = scale_y
     row.prop(export_settings, "export_subsurface_color", text="")
-    row.operator("matlay.export_subsurface_color", text="Export Subsurface Color", icon='RENDER_STILL')
+    row.label(text="Subsurface Color")
+    row.operator("matlay.export_subsurface_color", text="", icon='RENDER_STILL')
 
     row = layout.row()
     row.scale_y = scale_y
     row.prop(export_settings, "export_metallic", text="")
-    row.operator("matlay.export_metallic", text="Export Metallic", icon='RENDER_STILL')
+    row.label(text="Metallic")
+    row.operator("matlay.export_metallic", text="", icon='RENDER_STILL')
 
     row = layout.row()
     row.scale_y = scale_y
     row.prop(export_settings, "export_specular", text="")
-    row.operator("matlay.export_specular", text="Export Specular", icon='RENDER_STILL')
+    row.label(text="Specular")
+    row.operator("matlay.export_specular", text="", icon='RENDER_STILL')
 
     row = layout.row()
     row.scale_y = scale_y
     row.prop(export_settings, "export_roughness", text="")
-    row.operator("matlay.export_roughness", text="Export Roughness", icon='RENDER_STILL')
+    row.label(text="Roughness")
+    row.operator("matlay.export_roughness", text="", icon='RENDER_STILL')
 
     row = layout.row()
     row.scale_y = scale_y
     row.prop(export_settings, "export_normals", text="")
-    row.operator("matlay.export_normals", text="Export Normals", icon='RENDER_STILL')
+    row.label(text="Normals")
+    row.operator("matlay.export_normals", text="", icon='RENDER_STILL')
 
     row = layout.row()
     row.scale_y = scale_y
     row.prop(export_settings, "export_height", text="")
-    row.operator("matlay.export_height", text="Export Height", icon='RENDER_STILL')
+    row.label(text="Height")
+    row.operator("matlay.export_height", text="", icon='RENDER_STILL')
 
     row = layout.row()
     row.scale_y = scale_y
     row.prop(export_settings, "export_emission", text="")
-    row.operator("matlay.export_emission", text="Export Emission", icon='RENDER_STILL')
+    row.label(text="Emission")
+    row.operator("matlay.export_emission", text="", icon='RENDER_STILL')
 
     if not export_settings.show_advanced_settings:
         row = layout.row()
