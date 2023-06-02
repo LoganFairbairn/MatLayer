411387556b13fbdf85ce6f253d5c565d5924e543
Ryver <58080736+LoganFairbairn@users.noreply.github.com>
2023-06-02 10:40:03 -0400

    Incremented the add-on version to 1.0.5.

--- ui/main_ui.py
+++ ui/main_ui.py
@@ -29,7 +29,7 @@ class MATLAYER_panel_properties(bpy.types.PropertyGroup):
     )
 
 class MATLAYER_PT_Panel(bpy.types.Panel):
-    bl_label = "MatLayer " + "1.0.4"
+    bl_label = "MatLayer " + "1.0.5"
     bl_idname = "MATLAYER_PT_main_panel"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
