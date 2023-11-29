a86bf927e5dd260d6174041dd6408f1dbe7ad80c
Ryver <58080736+LoganFairbairn@users.noreply.github.com>
2023-11-29 08:51:30 -0500

    Incremented the add-on version to 2.0.3.

--- __init__.py
+++ __init__.py
@@ -58,7 +58,7 @@ from .core.subscription_update_handler import on_active_material_changed, on_act
 bl_info = {
     "name": "MatLayer",
     "author": "Logan Fairbairn (Ryver)",
-    "version": (2, 0, 2),
+    "version": (2, 0, 3),
     "blender": (4, 0, 0),
     "location": "View3D > Sidebar > MatLayer",
     "description": "Replaces node based texturing workflow with a layer stack workflow through a custom user interface.",
