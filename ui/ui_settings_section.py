eb6bc9b143fb7f119a69f1c7f1b872ee45c4db07
Ryver <logan-fairbairn@outlook.com>
2024-08-17 13:55:13 -0400

    Removed custom UI scaling from viewport tab.

--- ui/ui_settings_section.py
+++ ui/ui_settings_section.py
@@ -5,24 +5,19 @@ def draw_ui_settings_tab(self, context):
     ui_section_tabs.draw_addon_tabs(self, context)
 
     layout = self.layout
-    scale_y = 1.4
 
     split = layout.split(factor=0.3)
     first_column = split.column()
     second_column = split.column()
 
     row = first_column.row()
-    row.scale_y = scale_y
     row.label(text="Engine")
     row = second_column.row()
-    row.scale_y = scale_y
     row.prop(bpy.context.scene.render, 'engine', text="")
 
     row = first_column.row()
-    row.scale_y = scale_y
     row.label(text="View Transform")
     row = second_column.row()
-    row.scale_y = scale_y
     row.prop(bpy.context.scene.view_settings, "view_transform", text="")
 
     # Draw HDRI Settings
@@ -42,61 +37,45 @@ def draw_ui_settings_tab(self, context):
             hdri_node = hdri_world.node_tree.nodes.get('HDRI')
             if hdri_node:
                 row = first_column.row()
-                row.scale_y = scale_y
                 row.label(text="Background Color")
                 row = second_column.row()
-                row.scale_y = scale_y
                 row.prop(hdri_node.inputs[0], "default_value", text="")
 
                 row = first_column.row()
-                row.scale_y = scale_y
                 row.label(text="Environment Rotation")
                 row = second_column.row()
-                row.scale_y = scale_y
                 row.prop(hdri_node.inputs.get('Environment Rotation'), "default_value", text="", index=2, slider=True)
 
                 row = first_column.row()
-                row.scale_y = scale_y
                 row.label(text="Environment Blur")
                 row = second_column.row()
-                row.scale_y = scale_y
                 row.prop(hdri_node.inputs.get('Environment Blur'), "default_value", text="", slider=True)
 
                 row = first_column.row()
-                row.scale_y = scale_y
                 row.label(text="Environment Exposure")
                 row = second_column.row()
-                row.scale_y = scale_y
                 row.prop(hdri_node.inputs.get('Environment Exposure'), "default_value", text="", slider=True)
 
     # EEVEE Settings
     if bpy.context.scene.render.engine == 'BLENDER_EEVEE':
         row = first_column.row()
-        row.scale_y = scale_y
         row.label(text="EEVEE Viewport Samples")
         row = second_column.row()
-        row.scale_y = scale_y
         row.prop(bpy.context.scene.eevee, "taa_samples", text="")
 
         row = first_column.row()
-        row.scale_y = scale_y
         row.label(text="Bloom")
         row = second_column.row()
-        row.scale_y = scale_y
         row.prop(bpy.context.scene.eevee, 'use_bloom', text="")
 
         # Ambient Occlusion
         row = first_column.row()
-        row.scale_y = scale_y
         row.label(text="Ambient Occlusion")
         row = second_column.row()
-        row.scale_y = scale_y
         row.prop(bpy.context.scene.eevee, 'use_gtao', text="")
 
         # Screenspace Reflections
         row = first_column.row()
-        row.scale_y = scale_y
         row.label(text="Screen Space Reflections")
         row = second_column.row()
-        row.scale_y = scale_y
         row.prop(bpy.context.scene.eevee, 'use_ssr', text="")
