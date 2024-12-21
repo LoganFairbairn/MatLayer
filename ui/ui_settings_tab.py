45850245fdf74779618b192432b193aad589ecc9
Ryver <logan-fairbairn@outlook.com>
2024-12-21 15:29:37 -0500

    Removed duplicate unlayered shader property UI.

--- ui/ui_settings_tab.py
+++ ui/ui_settings_tab.py
@@ -105,7 +105,7 @@ def draw_shader_settings(layout):
             row.prop(selected_shader_channel, "socket_subtype", text="")
 
         row = first_column.row()
-        row.label(text="Default")
+        row.label(text="Default Value")
         row = second_column.row()
         match selected_shader_channel.socket_type:
             case 'NodeSocketFloat':
@@ -182,46 +182,6 @@ def draw_shader_settings(layout):
                 row.label(text="No Active Material")
         else:
             row.label(text="No Object Selected")
-
-    # Draw unlayered properties found in the active shader.
-    draw_unlayered_material_properties(layout)
-
-def draw_unlayered_material_properties(layout):
-    '''Draws all unlayered material properties.'''
-
-    # Ensure there is an active object.
-    active_object = bpy.context.active_object
-    if not active_object:
-        row.label(text="No Active Object Selected")
-        return
-
-    # Ensure there is an active material.
-    active_material = active_object.active_material
-    if not active_material:
-        row.label(text="No Active Material")
-        return
-
-    # Ensure there is a valid shader node.
-    matlayer_shader_node = active_material.node_tree.nodes.get('SHADER_NODE')
-    if not matlayer_shader_node:
-        row.label(text="No Valid Shader Node")
-        return
-
-    # Draw all unlayered material properties.
-    shader_info = bpy.context.scene.matlayer_shader_info
-    split = layout.split(factor=0.6)
-    first_column = split.column()
-    second_column = split.column()
-    for property in shader_info.unlayered_properties:
-        shader_property = matlayer_shader_node.inputs.get(property.name)
-        row = layout.row()
-        if shader_property:
-            row = first_column.row()
-            row.label(text=property.name)
-            row = second_column.row()
-            row.prop(shader_property, "default_value", text="")
-        else:
-            row.label(text="Shader Property Invalid")
 
 def draw_settings_tab(self, context):
     '''Draws user interface for the setup tab.'''
